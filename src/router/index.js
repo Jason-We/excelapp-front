8753827eea3be06802d5d23e00c4c584326dd133
Jason <1154844946@qq.com>
2020-09-06 00:37:57 +0800

    与后端交互

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Users from '../views/Users.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
@@ -17,6 +18,10 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }, {
+    path: '/users',
+    name: 'Users',
+    component: Users
   }
 ]
 
---
+++ src/views/Users.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="users">
+      <ul v-for="(user,index) in users" :key="index">
+         <li>{{user.name}}</li>
+         <li>{{user.sex}}</li>
+         <li>{{user.phone}}</li>
+         <li>{{user.others}}</li>
+      </ul>
+   </div>
+</template>
+
+<script>
+   export default {
+      name: "Users",
+      data() {
+         return {
+            users: {}
+         }
+      },
+      created() {
+         const that = this;
+         axios.get('http://localhost:8088/user/getAll/').then(function (res) {
+            console.log(res.data);
+            that.users = res.data;
+         })
+      },
+   }
+</script>
+<style scoped>
+   li {
+      list-style-type: none;
+   }
+</style>

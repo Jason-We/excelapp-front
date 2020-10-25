0287c9b991e0049a58a155917e561698ddebd818
Jason-We <1154844946@qq.com>
2020-10-25 23:43:09 +0800

    修改跨域配置

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  devServer: {
+  devServer: {   //这里解决开发环境的跨域问题， 生产环境的跨域问题需要再nginx服务器解决
     proxy: {
       '/api': {
         // 此处的写法，目的是为了 将 /api 替换成 http://localhost:8088/
@@ -12,7 +12,7 @@ module.exports = {
         changeOrigin: true,
         ws: true,
         pathRewrite: {
-          '^/api': '/'
+          '^/api': ''
         }
       }
     }
